bb45522dcc4ffffa84f003f667f948e342375fb6
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2024-05-01 23:41:44 +0530

    u

--- index.js
+++ index.js
@@ -16,69 +16,9 @@ function check_pass(event) {
 
 }
 
-// function create_card(pro_img, pro_name, pro_price) {
-//     let create_box = document.createElement("div");
-//     create_box.classList.add("cart_box");
-
-//     let create_picture = document.createElement("div");
-//     create_picture.classList.add("pic-adj");
-
-//     let add_img = document.createElement("img");
-//     add_img.classList.add("cart_img");
-//     add_img.setAttribute("src", pro_img);
-
-//     let add_pro_dt = document.createElement("div");
-//     add_pro_dt.classList.add("pro-adj");
-
-//     let add_hd1 = document.createElement("h3");
-//     add_hd1.classList.add("pro-h1");
-//     add_hd1.insertAdjacentText("afterbegin", pro_name);
 
 
-//     let add_hd2 = document.createElement("div");
-//     add_hd2.classList.add("pro-h2");
-//     add_hd2.insertAdjacentText("afterbegin", "price:" + pro_price);
-
-//     let add_btn = document.createElement("div");
-//     add_btn.classList.add("pro-btn-adj");
-
-//     let buy_btn = document.createElement("button");
-//     buy_btn.classList.add("pro-btn-buy");
-//     buy_btn.insertAdjacentText("afterbegin", "buy");
-
-
-//     let rmv_btn = document.createElement("button");
-//     rmv_btn.classList.add("pro-btn-rmv");
-//     rmv_btn.insertAdjacentText("afterbegin", "Remove");
-
-
-//     create_picture.append(add_img);
-//     create_box.append(create_picture);
-//     add_pro_dt.append(add_hd1);
-//     add_pro_dt.append(add_hd2);
-//     create_box.append(add_pro_dt);
-//     add_btn.append(buy_btn);
-//     add_btn.append(rmv_btn);
-//     create_box.append(add_btn);
-
-
-//     return create_box;
-
-// }
-
-
-// let btn = document.getElementById("add_to_cart")
-// btn.addEventListener("click", () => {
-//     let add_card = document.querySelector(".cart_container");
-
-//     add_card.append(create_card("shop images/organic-rice-amp-moong-khichdi-mix-200g-507933_l-removebg-preview.png", "Organic Rice & Moong Porridge Mix 200g", "350 rs"));
-
-
-// })
-// function openModal() {
-//     $('#cartdis').modal('show');
-//   }
-
+//addtocartfunction
 function add_to_cart(pid, pimg, ptitle, pprice) {
     let cart = localStorage.getItem("cart");
     if (cart == null) {
@@ -107,14 +47,61 @@ function add_to_cart(pid, pimg, ptitle, pprice) {
 
         }
     }
+    updateCart()
 }
 
+//cartdisplayfunction
 $('#openModal').click(function () {
-    $('#cartdis').modal('toggle'); 
-  });
+    $('#cartdis').modal('toggle');
+});
 
-  $(document).on('click', function (event) {
+$(document).on('click', function (event) {
     if ($(event.target).closest('.modal-content').length === 0 && $('#cartdis').hasClass('show')) {
-      $('#cartdis').modal('hide');
+        $('#cartdis').modal('hide');
     }
-  });
+});
+
+//updatecartfunction
+function updateCart() {
+    let cart_string = localStorage.getItem("cart");
+    let cart = JSON.parse(cart_string);
+    if (cart == null || cart.length == 0) {
+        console.log("cart is empty !!")
+
+        $(".cart-body").html('<h5 style="font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;">Cart Does Not Have Any Items </h5>')
+        $(".checkout-btn").addClass('disabled');
+
+    }
+    else {
+        console.log(cart)
+
+        let table = `
+        <table class="table">
+        <thead>
+        <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        </tr>
+        </thead>
+        
+        `;
+        table=`
+        <tr>
+        <td>${} </td>
+        <td>${} </td>
+        <td>${} </td>
+        <td>${} </td>
+        </tr>
+    `
+        let totalprice=0;
+        table += table; `</table>`
+        $(".cart-body").html(table)
+
+    }
+}
+
+$(document).ready(function () {
+    updateCart()
+})
