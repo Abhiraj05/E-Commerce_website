2299c4c100fcbe9b05ec389ea80f823013be7507
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2024-05-07 17:12:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,6 @@ $username = "root";
 $password = "";
 $dbname = "wtproject";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 
@@ -15,36 +14,36 @@ if ($conn->connect_error) {
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    
     $stmt = $conn->prepare("SELECT * FROM users WHERE full_name=? AND phone_no=? AND password=?");
     $stmt->bind_param("sss", $username, $phone, $password);
 
-
+    
     $username = $_POST['username'];
     $phone = $_POST['pno'];
     $password = $_POST['pass'];
 
+
     $stmt->execute();
+    
 
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-     
+      
         header("Location: shop.html");
         exit();
     } else {
-        
-        header("Location: signin.html?error=InvalidCredentials");
-        echo "invalid details!!"
+        echo '<script>alert("Invalid Details!!!")</script>';
+        header("Location: signin.html?");
         exit();
     }
 } else {
-
+    
     header("Location: signin.html");
-    echo "login successfully!";
-
     exit();
 }
+
 
 $stmt->close();
 $conn->close();
